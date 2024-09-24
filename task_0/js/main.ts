70f3a05c8f3a6d911e8ae385424d47119c354adf
nellyretto <danielys2416@gmail.com>
2024-09-24 13:05:08 -0400

    Add Student interface and student list

---
+++ task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: "",
+    lastName: "",
+    age: 777,
+    location: "",
+}
+
+const student2: Student = {
+    firstName: "",
+    lastName: "",
+    age: 777,
+    location: "",
+}
+
+const studentList: Student[] = [student1, student2]
